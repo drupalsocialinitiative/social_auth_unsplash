27535bad253c3fc1a01ae4abbbfad08b25704524
Getulio Sánchez <valentin2507@gmail.com>
2019-04-02 18:32:28 -0400

    Issue #3045313: Remove library check in the requirements hook

--- src/Plugin/Network/UnsplashAuthInterface.php
+++ src/Plugin/Network/UnsplashAuthInterface.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\social_auth_unsplash\Plugin\Network;
 
-use Drupal\social_api\Plugin\NetworkInterface;
+use Drupal\social_auth\Plugin\Network\NetworkInterface;
 
 /**
  * Defines the Unsplash Auth interface.
